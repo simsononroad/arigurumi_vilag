02b46b97e6b3f43e249cb1c5cadfd0e264fab8af
simsononroad <d-github@kopaszhegy.hu>
2024-11-15 19:58:35 +0100

    def

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pingvin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <table border="0">
+                <tr><td><img src="images/pingvin.jpeg" alt="Pingvin" title="Pingvin" width="300px"></td><td></td></tr>
+                <tr><th>Ár: 1.000HUF</th><td><form action="index.php" method="post">
+                    <input type="submit" value="További információk" name="pingvin_submit" id="">
+                </form></td></tr>
+        </table>
+    </div>
+</body>
+</html>
+
